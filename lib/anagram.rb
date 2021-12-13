d3081b455552a267bfd7939ac1a9590ddfb69c15
Mitch Toran <mitchellht34@gmail.com>
2021-12-13 10:20:14 -0500

    commit tree 251044a67c2e4455b75c0514b72414605569f55b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.filter do |anagram|
+            anagram.chars.sort == @word.chars.sort
+        end
+    end
+end
